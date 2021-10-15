520cfc849cd4ecd761c7afcd4e457dd2994781af
bhattmanav <manav.bhatt@icloud.com>
2021-10-15 22:09:41 +1030

    Added relevant products to the database and fixed menu.php

--- database.sql
+++ database.sql
@@ -55,17 +55,27 @@ GRANT all privileges ON comp2772.product TO dbadmin@localhost;
 --
 
 INSERT INTO `product` (`id`, `name`, `description`, `image`, `price`, `recommendedRetailPrice`, `category`, `keyWord`) VALUES
-('P000000001', 'USB to Lightning Cable', '', 'USB to Lightning Cable.png', '45.00', '45.00', 'cable', 'usb, lightning, cable'),
-('P000000002', '10W Wireless Charging Pad', '', 'Charging Pad.png', '49.95', '49.95', 'charger', '10w, wireless, charging, pad, charger'),
-('P000000003', '5W USB Power Adapter', '', '5W USB Power Adapter.png', '29.00', '29.00', 'charger', '5w, usb, power, adapter'),
-('P000000004', 'Headphones', '', 'Headphones.png', '349.00', '349.00', 'headphone', 'headphone, wireless, audio'),
-('P000000005', 'Earphones', '', 'Earphones.png', '249.00', '249.00', 'earphone', 'earphone, wireless, audio'),
-('P000000006', 'Power Bank', '', 'Power Bank.png', '79.95', '79.95', 'power bank', 'power, bank'),
-('P000000007', 'Silicone Case', '', 'Silicone Case.png', '29.95', '29.95', 'case', 'silicone, case'),
-('P000000008', 'Leather Case', '', 'Leather Case.png', '59.00', '59.00', 'case', 'leather, case'),
-('P000000009', 'Screen Protector', '', 'Screen Protector.png', '35.00', '35.00', 'screen protector', 'screen, protector'),
-('P000000010', 'Phone Holder', '', 'Phone Holder.png', '49.00', '49.00', 'holder', 'holder'),
-('P000000011', 'Airpods Pro', '', 'appleairpodsproduct.png', '299.00', '299.00', 'earphone', 'earphone`, `earphone`, `wireless`, `audio`, `apple');
+-- ('P000000001', 'USB to Lightning Cable', '', 'USB to Lightning Cable.png', '45.00', '45.00', 'cable', 'usb, lightning, cable'),
+-- ('P000000002', '10W Wireless Charging Pad', '', 'Charging Pad.png', '49.95', '49.95', 'charger', '10w, wireless, charging, pad, charger'),
+-- ('P000000003', '5W USB Power Adapter', '', '5W USB Power Adapter.png', '29.00', '29.00', 'charger', '5w, usb, power, adapter'),
+-- ('P000000004', 'Headphones', '', 'Headphones.png', '349.00', '349.00', 'headphone', 'headphone, wireless, audio'),
+-- ('P000000005', 'Earphones', '', 'Earphones.png', '249.00', '249.00', 'earphone', 'earphone, wireless, audio'),
+-- ('P000000006', 'Power Bank', '', 'Power Bank.png', '79.95', '79.95', 'power bank', 'power, bank'),
+-- ('P000000007', 'Silicone Case', '', 'Silicone Case.png', '29.95', '29.95', 'case', 'silicone, case'),
+-- ('P000000008', 'Leather Case', '', 'Leather Case.png', '59.00', '59.00', 'case', 'leather, case'),
+-- ('P000000009', 'Screen Protector', '', 'Screen Protector.png', '35.00', '35.00', 'screen protector', 'screen, protector'),
+-- ('P000000010', 'Phone Holder', '', 'Phone Holder.png', '49.00', '49.00', 'holder', 'holder'),
+-- ('P000000011', 'Airpods Pro', '', 'appleairpodsproduct.png', '299.00', '299.00', 'earphone', 'earphone`, `earphone`, `wireless`, `audio`, `apple'),
+('P000000001', 'Bose QuietComfort 45 Wireless Noise Cancelling Headphones', '', 'qc45.png', '550', '499.00', 'headphones', 'earphone`, `headphones`, `wireless`, `audio`, `bose'),
+('P000000002', 'Xbox Elite Wireless Controller Series 2', '', 'xbox.png', '275', '249.00', 'gaming', 'controller`, `gaming`, `wireless`, `elite'),
+('P000000003', 'Sony ZV-1 Digital Camera (White)', '', 'cameraimage.png', '1049', '899.00', 'cameras', 'digital`, `camera`, `white'),
+('P000000004', 'iPhone 12 Mini 128GB Black', '', 'mb1.png', '1079', '1079.00', 'mobile-phones', 'iPhone`, `mobile`, `phone`, `wireless`, `mini'),
+('P000000005', 'DJI Air 2S 4K Drone Fly More Combo', '', 'djdrone.png', '3050', '2099.00', 'dcgha', 'air`, `drone`, `4k`, `fly`, `combo'),
+('P000000006', 'Apple TV 4K 32GB [2021]', '', 'appletv.png', '249', '249.00', 'tvs', 'tv`, `4k`, `32GB`, `remote`, `2021'),
+('P000000007', 'Apple Watch Series 3 38mm Silver Aluminium Case GPS', '', 'applewatch.png', '299', '299.00', 'hfw', 'watch`, `silver`, `aluminium`, `health`, `fitness'),
+('P000000008', 'Microsoft Surface Laptop 4 13.5" i7 512GB/16GB (Matte Black)', '', 'MicrosoftGo.png', '2600', '2549.00', 'computers', 'surface`, `laptop`, `matte`, `black`, `512GB'),
+('P000000009', 'Theragun Mini White Massage Gun', '', 'massageegun.png', '400', '349.00', 'hfw', 'mini`, `white`, `massage`, `gun`, `health`, `fitness'),
+('P000000010', 'Google - Nest Learning Smart Wifi Thermostat - Stainless Steel', '', 'googlenest.png', '350', '299.00', 'dcgha', 'nest`, `thermostat`, `home`, `appliances`, `stainless-steel');
 
 --
 -- Indexes for dumped tables
--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <!-- Header - Top and Bottom -->
-<div class="first-header" id="first-header"></div>
+<!-- <div class="first-header" id="first-header"></div>
 <div class="second-header" id="second-header">
     <header class="header-second">
     <a href="index.php"> <img class="header2-logo" src="images/header-logo.png" alt="logo" /> </a>
@@ -32,5 +32,55 @@
         </button>
     </a>
     </header>
-</div>
+</div> -->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="styles/menufinal.css" />
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="first-header" id="first-header"></div>
+
+    <header>
+      <div class="second-header-container">
+        <a class="header-logo-link" href="index.php"><img class="header-logo" src="images/header-logo.png" alt="" /></a>
+        <nav>
+          <ul class="nav--links">
+            <li class="second-header-links" id="list"><a href=""></a>Products</li>
+
+            <li id="list">
+              <form action="products.php" id="formSearch" name="formSearch" method="POST">
+                <input class="second-header-search-field" type="text" id="textSearch" name="search" placeholder="Search" />
+               
+                <select id="selectCategory" name="category">
+                  <option value="all categories">All Categories</option>
+                  <option value="computers">Computers</option>
+                  <option value="tvs">TVs</option>
+                  <option value="headphones">Headphones</option>
+                  <option value="mobile-phones">Mobile Phones</option>
+                  <option value="gaming">Gaming</option>
+                  <option value="cameras">Cameras</option>
+                  <option value="dcgha">Drones / Car Gear / Home Appliances</option>
+                  dcgha = drones / car gear / home appliances
+                  <option value="hfw">Health Fitness & Wearables</option>
+                  hfw = health fitness & wearables
+                </select>
+                <input class="second-header-search-button" type="submit" value="Search" />
+              </form>
+            </li>
+
+            <li class="second-header-links" id="list">
+              <a id="menuCartLink" href="cart.php"><?php echo (isset($_SESSION["products"]) ? " (" . count($_SESSION["products"]) . ")" : ""); ?><button class="my-cart-button">My Cart</button></a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </body>
+</html>
 
