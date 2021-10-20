b60c495113d9a4462852e3e25787df1420e15901
Adam Eustis <eust0015@flinders.edu.au>
2021-10-20 17:26:03 +1030

    Add message (Again) that notifies when no search results were found

--- products.php
+++ products.php
@@ -85,8 +85,12 @@
                     <?php  
                     }
                 }
+                else{
+                    echo "<br><br>";
+                    echo "No products in '" . ucwords($_POST["category"]) . "' matching search for '" . $_POST["search"] . "'";
+                }
                 mysqli_close($conn);
-            }           
+            }         
         ?>
     </body>
 </html>
