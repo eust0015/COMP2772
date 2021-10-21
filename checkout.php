67a15874c27ce40ab2b79614529914ddd0387e81
bhattmanav <manav.bhatt@icloud.com>
2021-10-21 14:04:15 +1030

    Added additional styling to the checkout.php page.

--- checkout.php
+++ checkout.php
@@ -6,7 +6,8 @@
 <html>
     <head>
         <link rel="stylesheet" href="styles/menu.css">
-        <link rel="stylesheet" href="styles/style.css">
+        <link rel="stylesheet" href="styles/checkouttest.css" />
+        <!-- <link rel="stylesheet" href="styles/style.css"> -->
         <script src="scripts/checkout.js" defer></script>
         <meta charset="utf-8">
         <meta name="author" content="Group-07" />
@@ -18,8 +19,6 @@
             include_once 'menu.php'; 
         ?>     
         
-        <div id='heading'><h1>Checkout</h1></div>
-
         <?php 
             include_once 'db_functions.php';
             $conn = get_conn();
@@ -30,58 +29,41 @@
                 
         ?>
         
+        
         <!-- Billing informormation -->
         <form id='checkout-form' name='customerDetails' class='checkout-form' action='payment.php' method='POST'>
-            <h3>Billing Information</h3>
-                <div name='customerDetails'>
-                    <ul class='customerDetails'>
-                        <li><label id='billing-fname' for='billing-fname'>First Name: </label><input type='text' name='billing-fname' class='billing-address' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["billing-fname"]) ? $_SESSION["account"]["billing-fname"] : ""); ?>'></li>
-                        <li><label id='billing-lname' for='billing-lname'>Last Name: </label><input type='text' name='billing-lname' class='billing-address' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["billing-lname"]) ? $_SESSION["account"]["billing-lname"] : ""); ?>'></li>
-                        <li><label id='billing-mobilenumber' for='billing-mobilenumber'>Mobile number: </label><input type='text' name='billing-mobilenumber' class='billing-address' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["billing-mobilenumber"]) ? $_SESSION["account"]["billing-mobilenumber"] : ""); ?>'></li>
-                        <li><label id='billing-email' for='billing-email'>Email Address: </label><input type='email' name='billing-email' class='billing-address' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["billing-email"]) ? $_SESSION["account"]["billing-email"] : ""); ?>'></li><br><br><br><br>
-                        <li><label id='billing-streetAddress' for='billing-streetAddress'>Street Address: </label><input type='text' name='billing-streetAddress' class='billing-address' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["billing-streetAddress"]) ? $_SESSION["account"]["billing-streetAddress"] : ""); ?>'></li>
-                        <li><label id='billing-suburb' for='billing-suburb'>Suburb: </label><input type='text' name='billing-suburb' class='billing-address' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["billing-suburb"]) ? $_SESSION["account"]["billing-suburb"] : ""); ?>'></li>
-                        <li><label id='billing-state'for='billing-state'> State: </label><select name='billing-state' class='billing-address' value='<?php echo (isset($_SESSION["account"]["billing-state"]) ? $_SESSION["account"]["billing-state"] : ""); ?>'>
-                        <option value='NSW'>NSW</option>
-                        <option value='ACT'>ACT</option>
-                        <option value='VIC'>VIC</option>
-                        <option value='QLD'>QLD</option>
-                        <option value='TAS'>TAS</option>
-                        <option value='NT'>NT</option>
-                        <option value='SA'>SA</option>
-                        <option value='WA'>WA</option></select>
-                        <li><label id='billing-postcode' for='billing-postcode'>Post Code: <input type='text' name='billing-postcode' class='billing-address' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["billing-postcode"]) ? $_SESSION["account"]["billing-postcode"] : ""); ?>'></label></li>
-                    </ul>
+            <div class="first-section-checkout-container">
+              <div class="first-section-shipping-information">
+                <div class="first-section-checkout-container-heading">
+                  <span class="first-section-checkout-heading">Getting your order </span>
                 </div>
-
-            <!-- Shipping information -->
-            <h3 id='shipping-header'>Shipping Information</h3>
-                <li><input type='checkbox' id='shippingAddressCheckBox' name='shipToBillingAddress' <?php echo (isset($_SESSION["account"]["shipToBillingAddress"]) && $_SESSION["account"]["shipToBillingAddress"] ? "checked" : ""); ?>><label id='shippingAddress' for='shippingAddress'>Shipping Address Same As Billing Address</li><br><br><br>
-                <div id='shippingAddressDiv' name='shippingAddressDiv'>
-                    <div name='customerDetails'>
-                        <ul class='customerDetails'>
-                        <li><label id='shipping-fname' for='shipping-fname'>First Name: </label><input type='text' name='shipping-fname' class='shipping-address' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["shipping-fname"]) ? $_SESSION["account"]["shipping-fname"] : ""); ?>'></li>
-                        <li><label id='shipping-lname' for='shipping-lname'>Last Name: </label><input type='text' name='shipping-lname' class='shipping-address' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["shipping-lname"]) ? $_SESSION["account"]["shipping-lname"] : ""); ?>'></li>
-                        <li><label id='shipping-mobilenumber' for='shipping-mobilenumber'>Mobile number: </label><input type='text' class='shipping-address' name='shipping-mobilenumber' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["shipping-mobilenumber"]) ? $_SESSION["account"]["shipping-mobilenumber"] : ""); ?>'></li>
-                        <li><label id='shipping-email' for='shipping-email'>Email Address: </label><input type='email' name='shipping-email' class='shipping-address' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["shipping-email"]) ? $_SESSION["account"]["shipping-email"] : ""); ?>'></li><br><br><br><br>
-                        <li><label id='shipping-streetAddress' for='shipping-streetAddress'>Street Address: </label><input type='text' name='shipping-streetAddress' class='shipping-address' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["shipping-streetAddress"]) ? $_SESSION["account"]["shipping-streetAddress"] : ""); ?>'></li>
-                        <li><label id='shipping-suburb' for='shipping-suburb'>Suburb: </label><input type='text' name='shipping-suburb' class='shipping-address' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["shipping-suburb"]) ? $_SESSION["account"]["shipping-suburb"] : ""); ?>'></li>
-                        <li><label id='shipping-state'for='state'> State: <select name='shipping-state' class='shipping-address' value='<?php echo (isset($_SESSION["account"]["shipping-state"]) ? $_SESSION["account"]["shipping-state"] : ""); ?>'>
-                        <option value='NSW'>NSW</option>
-                        <option value='ACT'>ACT</option>
-                        <option value='VIC'>VIC</option>
-                        <option value='QLD'>QLD</option>
-                        <option value='TAS'>TAS</option>
-                        <option value='NT'>NT</option>
-                        <option value='SA'>SA</option>
-                        <option value='WA'>WA</option></select>
-                        <li><label id='shipping-postcode' for='shipping-postcode'>Post Code: <input type='text' name='shipping-postcode' class='shipping-address' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["shipping-postcode"]) ? $_SESSION["account"]["shipping-postcode"] : ""); ?>'></label></li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- Postage options -->
-                <h3 id='postage-header'>Postage Option</h3>
-                <div name='postageOptions'>
+                <div class="first-section-shipping">
+                  <div class="first-section-shipping-details">
+                    <span class="section-subheadings">Billing Information </span>
+                    <span class="first-section-shipping-form">First Name </span>
+                    <input class="first-section-shipping-form-input-fields" type="text" name='billing-fname' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["billing-fname"]) ? $_SESSION["account"]["billing-fname"] : ""); ?>' />
+                    <span class="first-section-shipping-form">Last Name </span>
+                    <input class="first-section-shipping-form-input-fields" type="text" name='billing-lname' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["billing-lname"]) ? $_SESSION["account"]["billing-lname"] : ""); ?>' />
+                    <span class="first-section-shipping-form">Address </span>
+                    <input class="first-section-shipping-form-input-fields" type="text" name='billing-streetAddress' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["billing-streetAddress"]) ? $_SESSION["account"]["billing-streetAddress"] : ""); ?>' />
+                    <span class="first-section-shipping-form">Suburb </span>
+                    <input class="first-section-shipping-form-input-fields" type="text" name='billing-suburb' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["billing-suburb"]) ? $_SESSION["account"]["billing-suburb"] : ""); ?>' />
+                    <span class="first-section-shipping-form">State </span>
+                    <select class="first-section-shipping-form-input-fields" name='billing-state' value='<?php echo (isset($_SESSION["account"]["billing-state"]) ? $_SESSION["account"]["billing-state"] : ""); ?>' >
+                      <option value="NSW">NSW</option>
+                      <option value="ACT">ACT</option>
+                      <option value="VIC">VIC</option>
+                      <option value="QLD">QLD</option>
+                      <option value="TAS">TAS</option>
+                      <option value="NT">NT</option>
+                      <option value="SA">SA</option>
+                      <option value="WA">WA</option>
+                    </select>
+                    <span class="first-section-shipping-form">Postcode </span>
+                    <input class="first-section-shipping-form-input-fields" type="text" name='billing-postcode' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["billing-postcode"]) ? $_SESSION["account"]["billing-postcode"] : ""); ?>' />
+                  </div>
+                  <div class="first-section-shipping-information-postage-options">
+                    <span class="section-subheadings">Postage Options</span>
                     <?php
                         $results = get_postages($conn);
                         if ($results) { 
@@ -96,46 +78,69 @@
                                 else{
                                     echo "<input id='" . $results[$row]["id"] . "' type='radio' name='postage' value='" . $results[$row]["id"] . "'>";
                                 }
-                                echo "<label for='" . $results[$row]["id"] . "'>" . $results[$row]["name"] . ": $" . $results[$row]["cost"] . "</label>";
+                                echo "<span class='postageCostText' for='" . $results[$row]["id"] . "'>" . $results[$row]["name"] . ": $" . $results[$row]["cost"] . "</span>";
                                 echo "<input type='hidden' id='cost_" . $results[$row]["id"] . "' value='" . $results[$row]["cost"] . "'>";
                                 echo "<br><br>";
                             }
                         }
                     ?>
+                  </div>
                 </div>
-
-            <!-- Order summary -->
-            <h3>Order Summary</h3>
-                <?php
-                foreach($_SESSION["products"] as $productId => $productQuantity) {
-                    $result = get_product_by_id($conn, htmlspecialchars($productId));
-                    if ($result) {
-                        echo "<div id='checkoutSummary>";
-                        $subTotal = $productQuantity * $result["price"];
-                        $total += $subTotal;
-                        echo "<div id=checkoutProductDetails>";
-                        echo "<div class='product-item-details'>";
-                        echo "<img src='images/" . $result["image"] . "' alt='" . $result["name"] . ">";
-                        echo "</div>";
-                        echo "<div id='productItemName>";
-                        echo "<strong class='product-item-name'><br>" . $result["name"] . "</strong><br>";
-                        echo "<strong>Item Price: $" . $result["price"];
-                        echo "<div id='checkoutQuantity'>";
-                        echo "<strong>Quantity: </strong>" . $productQuantity;
-                        echo "</div>";
-                        echo "<strong>Subtotal: </strong>$" . number_format((float)$subTotal, 2);
+              </div>
+              <div class="first-section-order-summary">
+                  <?php
+                    foreach($_SESSION["products"] as $productId => $productQuantity) {
+                        $result = get_product_by_id($conn, htmlspecialchars($productId));
+                        if ($result) {
+                          echo "<div class='checkoutSummary'>";
+                            $subTotal = $productQuantity * $result["price"];
+                            $total += $subTotal;
+                            echo "<img src='images/" . $result["image"] . "' alt='" . $result["name"] . ">";
+                            echo "<span class='checkoutSummaryFields'><br>" . $result["name"] . "</span><br>";
+                            echo "<span class='checkoutSummaryFields'>Item Price: $" . $result["price"] . "</span>";
+                            echo "<span class='checkoutSummaryFields'>Quantity: " . $productQuantity . "</span>";
+                            echo "<span class='checkoutSummaryFields'>Total Price: " . number_format((float)$subTotal, 2) . "</span>";
+                            echo "</div>";
+                        }
+                    }
+                    echo "<div id='totalAmount'>";
+                    echo "<input type='hidden' id='cartTotalCost' name='cartTotalCost' value='" . $total . "'>";
+                    echo "<br><br><strong id='grandTotalCost'>Total Amount: $" . number_format((float)$total + (float)$postageCost, 2, '.', '') . "</strong>";
+                    echo "</div>";
                     }
                 }
-                echo "<div id='totalAmount'>";
-                echo "<input type='hidden' id='cartTotalCost' name='cartTotalCost' value='" . $total . "'>";
-                echo "<br><br><strong id='grandTotalCost'>Total Amount: $" . number_format((float)$total + (float)$postageCost, 2, '.', '') . "</strong>";
-                echo "</div>";
-                }
-            }
-            ?>
-            <div id='proceed-to-checkout-button'>
-                <input type='hidden' id='accountAction' name='accountAction' value='update'>
-                <input type='submit' id='proceed-to-payment' value='Proceed To Payment'>
+                ?>
+              </div>
+            </div>
+
+            <!-- Shipping information -->
+            <div class="second-section-checkout-container">
+              <div class="second-section-billing-information">
+                <span class="section-subheadings">Shipping Information </span>
+                <span class="first-section-shipping-form">First Name </span>
+                <input class="first-section-shipping-form-input-fields" type="text" name='shipping-fname' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["shipping-fname"]) ? $_SESSION["account"]["shipping-fname"] : ""); ?>' />
+                <span class="first-section-shipping-form">Last Name </span>
+                <input class="first-section-shipping-form-input-fields" type="text" name='shipping-lname' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["shipping-lname"]) ? $_SESSION["account"]["shipping-lname"] : ""); ?>' />
+                <span class="first-section-shipping-form">Address </span>
+                <input class="first-section-shipping-form-input-fields" type="text" name='shipping-streetAddress' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["shipping-streetAddress"]) ? $_SESSION["account"]["shipping-streetAddress"] : ""); ?>' />
+                <span class="first-section-shipping-form">Suburb </span>
+                <input class="first-section-shipping-form-input-fields" type="text" name='shipping-suburb' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["shipping-suburb"]) ? $_SESSION["account"]["shipping-suburb"] : ""); ?>' />
+                <span class="first-section-shipping-form">State </span>
+                <select class="first-section-shipping-form-input-fields" name='shipping-state' value='<?php echo (isset($_SESSION["account"]["shipping-state"]) ? $_SESSION["account"]["shipping-state"] : ""); ?>' >
+                  <option value="NSW">NSW</option>
+                  <option value="ACT">ACT</option>
+                  <option value="VIC">VIC</option>
+                  <option value="QLD">QLD</option>
+                  <option value="TAS">TAS</option>
+                  <option value="NT">NT</option>
+                  <option value="SA">SA</option>
+                  <option value="WA">WA</option>
+                </select>
+                <span class="first-section-shipping-form">Postcode </span>
+                <input class="first-section-shipping-form-input-fields" type="text" name='shipping-postcode' placeholder='Required' required value='<?php echo (isset($_SESSION["account"]["shipping-postcode"]) ? $_SESSION["account"]["shipping-postcode"] : ""); ?>' />
+                <input type="hidden" id="accountAction" name="accountAction" value="update" />
+                <input class="section-section-button-proceed-to-payment" type="submit" id="proceed-to-payment" value="Proceed To Payment" />
+              </div>
             </div>
         </form>
     </body>
